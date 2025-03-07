73b1ecf045b15cf731e0593979ae6fb2904417bd
sungwook <respectwo@naver.com>
2025-03-07 15:47:07 +0900

    feawt: backend init

---
+++ backend/src/main/java/org/shax3/square/SquareApplication.java
@@ -0,0 +1,13 @@
+package org.shax3.square;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquareApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/shax3/square/SquareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shax3.square;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
