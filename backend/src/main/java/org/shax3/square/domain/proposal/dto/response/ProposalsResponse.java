02ae745ca61f8b1a118158ac0658fa09d3be3dfe
sungwook <respectwo@naver.com>
2025-03-17 16:47:09 +0900

    :sparkles: feat: 청원 목록 조회(좋아요, 최신순) 구현

--- backend/src/main/java/org/shax3/square/domain/proposal/dto/response/ProposalsResponse.java
+++ backend/src/main/java/org/shax3/square/domain/proposal/dto/response/ProposalsResponse.java
@@ -2,9 +2,15 @@ package org.shax3.square.domain.proposal.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.shax3.square.domain.proposal.dto.ProposalDto;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class ProposalsResponse {
-    public Long proposalId;
+    private List<ProposalDto> proposals;
+    private Long nextCursorId;
+    private Integer nextCursorLikes;
 }
+
