4a072fefd13c8a4abfa0b238688db85ebe5efdf2
Junho-Seo <sjh2395@gmail.com>
2025-03-17 10:51:32 +0900

    :recycle: refactor: 미사용 기본 폴더 삭제

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { SampleScreen, UiTestScreen } from "../screens";
+import { SampleScreen, UiTestScreen } from "../pages";
 import { SampleButton } from "../components";
 
 export default function App() {
