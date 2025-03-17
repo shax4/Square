5a23e4d964903f0c48c671938d5cc8a0e7050af2
sungwook <respectwo@naver.com>
2025-03-17 16:46:21 +0900

    :wrench: config: QueryDslConfig 추가

--- backend/src/main/java/org/shax3/square/config/QueryDslConfig.java
+++ backend/src/main/java/org/shax3/square/config/QueryDslConfig.java
@@ -3,17 +3,10 @@ package org.shax3.square.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaAuditing
-@EnableJpaRepositories(
-        basePackages = "org.shax3.square.domain")
-@EntityScan(basePackages = "org.shax3.square.domain")
 public class QueryDslConfig {
 
     @PersistenceContext
