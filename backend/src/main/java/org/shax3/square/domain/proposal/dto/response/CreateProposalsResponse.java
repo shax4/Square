89c7216785ec7dc1f1e965087c970c59297ed28f
sungwook <respectwo@naver.com>
2025-03-17 17:29:51 +0900

    :recycle: refactor: sort 가공 로직 메서드 분리

--- backend/src/main/java/org/shax3/square/domain/proposal/dto/response/CreateProposalsResponse.java
+++ backend/src/main/java/org/shax3/square/domain/proposal/dto/response/CreateProposalsResponse.java
@@ -1,8 +1,10 @@
 package org.shax3.square.domain.proposal.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @Getter
 @AllArgsConstructor
 public class CreateProposalsResponse {
