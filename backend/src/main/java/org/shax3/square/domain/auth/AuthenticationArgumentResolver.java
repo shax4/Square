d6d94842c50558e3e9c1c28feb96815e42bc6509
윤다은 <syoon4486@gmail.com>
2025-03-17 17:51:59 +0900

    :sparkles: feat: accessToken 검증 어노테이션 기능 개발

---
+++ backend/src/main/java/org/shax3/square/domain/auth/AuthenticationArgumentResolver.java
@@ -0,0 +1,83 @@
+package org.shax3.square.domain.auth;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.shax3.square.domain.auth.annotation.AuthUser;
+import org.shax3.square.domain.user.model.User;
+import org.shax3.square.domain.user.repository.UserJpaRepository;
+import org.shax3.square.exception.CustomException;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Arrays;
+
+import static org.shax3.square.exception.ExceptionCode.*;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
+    private final UserJpaRepository userJpaRepository;
+    private final TokenUtil tokenUtil;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthUser.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+
+        if (request == null) {
+            throw new CustomException(FAILED_TO_VALIDATE_TOKEN);
+        }
+
+        String accessToken = extractAccessToken(request);
+
+        if (tokenUtil.isAccessTokenValid(accessToken)) {
+            return extractUser(accessToken);
+        }
+
+        throw new CustomException(FAILED_TO_VALIDATE_TOKEN);
+    }
+
+    private String extractAccessToken(HttpServletRequest request) {
+        String authHeader = request.getHeader(AUTHORIZATION);
+        if (authHeader == null) {
+            throw new CustomException(INVALID_ACCESS_TOKEN);
+        }
+        return authHeader.split(" ")[1];
+    }
+
+    private String extractRefreshToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies == null) {
+            throw new CustomException(INVALID_REFRESH_TOKEN);
+        }
+
+        return Arrays.stream(cookies)
+            .filter(cookie -> cookie.getName().equals("refresh-token"))
+            .findFirst()
+            .orElseThrow(() -> new CustomException(INVALID_REFRESH_TOKEN))
+            .getValue();
+    }
+
+    private User extractUser(String accessToken) {
+        Long userId = Long.valueOf(tokenUtil.getSubject(accessToken));
+
+        return userJpaRepository.findById(userId)
+            .orElseThrow(() -> new CustomException(INVALID_REQUEST));
+    }
+}
--- backend/src/main/java/org/shax3/square/domain/auth/TokenUtil.java
+++ backend/src/main/java/org/shax3/square/domain/auth/TokenUtil.java
@@ -1,5 +1,8 @@
 package org.shax3.square.domain.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.shax3.square.domain.auth.domain.RefreshToken;
@@ -24,7 +27,6 @@ public class TokenUtil {
             @Value("${spring.auth.jwt.secret-key}") final String secretKey,
             @Value("${spring.auth.jwt.access-token-expiry}") final Long accessTokenExpiry,
             @Value("${spring.auth.jwt.refresh-token-expiry}") final Long refreshTokenExpiry
-
     ) {
         this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
         this.accessTokenExpiry = accessTokenExpiry;
@@ -56,5 +58,25 @@ public class TokenUtil {
         byte[] tokenBytes = new byte[32];
         secureRandom.nextBytes(tokenBytes);
         return Base64.getUrlEncoder().withoutPadding().encodeToString(tokenBytes);
+    }
+
+    public boolean isAccessTokenValid(String accessToken) {
+        try {
+            parseToken(accessToken);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    private Jws<Claims> parseToken(String accessToken) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(accessToken);
+    }
+
+    public String getSubject(String accessToken) {
+        return parseToken(accessToken).getBody().getSubject();
     }
 }
