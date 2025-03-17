f34eaf152619099d3a83ed1fab260d526012e58a
배석진 <bsj1044@naver.com>
2025-03-17 15:04:23 +0900

    🚧 chore: 상단 탭 바 개발

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HeaderBar from '../components/header-bar/HeaderBar';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>헬로우!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <HeaderBar />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
