7620f145cf9883ba062781c67811c2c5fab38bc4
sungwook <respectwo@naver.com>
2025-03-17 13:35:32 +0900

    :construction: chore: 청원 목록 조회 기능 개발중

--- backend/src/main/java/org/shax3/square/domain/proposal/controller/ProposalController.java
+++ backend/src/main/java/org/shax3/square/domain/proposal/controller/ProposalController.java
@@ -4,12 +4,15 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.shax3.square.domain.proposal.dto.request.CreateProposalRequest;
 import org.shax3.square.domain.proposal.dto.response.ProposalResponse;
+import org.shax3.square.domain.proposal.dto.response.ProposalsResponse;
 import org.shax3.square.domain.proposal.service.ProposalService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/proposal")
+@RequestMapping("/proposals")
 @RequiredArgsConstructor
 public class ProposalController {
     private final ProposalService proposalService;
@@ -19,7 +22,19 @@ public class ProposalController {
                                                            @RequestHeader("Authorization") String token) {
         ProposalResponse response = proposalService.save(request, token);
         return ResponseEntity.ok(response);
-
-
     }
+
+
+    @GetMapping
+    public ResponseEntity<ProposalsResponse> getProposals(
+            @RequestParam(defaultValue = "latest") String sort,
+            @RequestParam(required = false) Long nextCursorId,
+            @RequestParam(required = false) Long nextCursorLikes,
+            @RequestParam(defaultValue = "5") int limit
+    ) {
+//        ProposalsResponse response = proposalService.getProposals(sort, nextCursorId, nextCursorLikes, limit);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
