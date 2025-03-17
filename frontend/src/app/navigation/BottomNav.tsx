4eb9d7287221bec61fda071a28de954a1e4bd4e8
김민철 <alscjf0327@naver.com>
2025-03-17 11:09:03 +0900

    ✨ feat : 하단 네비게이션 바와 스택 구조 병합

--- frontend/src/app/navigation/BottomNav.tsx
+++ frontend/src/app/navigation/BottomNav.tsx
@@ -3,7 +3,9 @@ import { createBottomTabNavigator  } from "@react-navigation/bottom-tabs";
 import {Ionicons} from '@expo/vector-icons';
 
 import {BoardScreen, MainScreen, MypageScreen, NotificationScreen} from '../../pages'
+import HeaderBar from './stacks/HeaderBar'
 
+// 하단에 표시되는 탭 이름.
 const boardName = '게시판';
 const mainName = '메인 홈';
 const notificationName = '알림';
@@ -42,11 +44,27 @@ export default function BottomNav(){
                         />
                     );
                     },
+                    tabBarStyle: {
+                        position: "absolute", // 바를 떠있는 느낌으로 만들기 위해 필요
+                        backgroundColor: "white", // 배경색 설정
+                        borderRadius: 20, // 네비게이션 바의 모서리를 둥글게
+                        marginHorizontal: 20, // 좌우 여백 추가
+                        marginBottom: 10, // 아래 여백 추가
+                        marginTop : 10,
+                        height: 60, // 네비게이션 바 높이 조정
+                        shadowColor: "#000", // 그림자 효과
+                        shadowOffset: { width: 0, height: 5 },
+                        shadowOpacity: 0.1,
+                        shadowRadius: 10,
+                        elevation: 5, // 안드로이드 그림자 효과
+                      },
+                    tabBarActiveTintColor: "black",
+                    tabBarInactiveTintColor: "gray",
                     headerShown: false,
                 })}
                 >
 
-                <Tab.Screen name={boardName} component={BoardScreen}/>
+                <Tab.Screen name={boardName} component={HeaderBar}/>
                 <Tab.Screen name={mainName} component={MainScreen}/>
                 <Tab.Screen name={notificationName} component={NotificationScreen}/>
                 <Tab.Screen name={mypageName} component={MypageScreen}/>
