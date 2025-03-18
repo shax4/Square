cfb06e65a6836183988de9d92b2f04a247d09fd8
sungwook <respectwo@naver.com>
2025-03-18 14:50:45 +0900

    :bug: fix: 상속관계 수정

--- backend/src/main/java/org/shax3/square/domain/opinion/repository/OpinionRepository.java
+++ backend/src/main/java/org/shax3/square/domain/opinion/repository/OpinionRepository.java
@@ -1,7 +1,8 @@
 package org.shax3.square.domain.opinion.repository;
 
 import org.shax3.square.domain.debate.model.Debate;
+import org.shax3.square.domain.opinion.model.Opinion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OpinionRepository extends JpaRepository<Debate, Long> {
+public interface OpinionRepository extends JpaRepository<Opinion, Long> {
 }
