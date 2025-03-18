f2f5a7737ad40cdc840bc58a7c3bb18ad5be2b54
sungwook <respectwo@naver.com>
2025-03-18 12:49:43 +0900

    :recycle: refactor: DTO records로 변경

--- backend/src/main/java/org/shax3/square/domain/proposal/dto/response/CreateProposalsResponse.java
+++ backend/src/main/java/org/shax3/square/domain/proposal/dto/response/CreateProposalsResponse.java
@@ -1,12 +1,4 @@
 package org.shax3.square.domain.proposal.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Builder
-@Getter
-@AllArgsConstructor
-public class CreateProposalsResponse {
-    public Long proposalId;
+public record CreateProposalsResponse(Long proposalId) {
 }
--- backend/src/main/java/org/shax3/square/domain/proposal/dto/response/ProposalsResponse.java
+++ backend/src/main/java/org/shax3/square/domain/proposal/dto/response/ProposalsResponse.java
@@ -1,19 +1,11 @@
 package org.shax3.square.domain.proposal.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 import org.shax3.square.domain.proposal.dto.ProposalDto;
-
 import java.util.List;
 
-@Builder
-@Getter
-@AllArgsConstructor
-public class ProposalsResponse {
-    private List<ProposalDto> proposals;
-    private Long nextCursorId;
-    private Integer nextCursorLikes;
-
+public record ProposalsResponse(
+        List<ProposalDto> proposals,
+        Long nextCursorId,
+        Integer nextCursorLikes
+) {
 }
-
