e4ac64b7f8cad228edd2134760c0132272794784
sungwook <respectwo@naver.com>
2025-03-19 11:09:38 +0900

    :memo: docs: 청원 삭제API description 추가

--- backend/src/main/java/org/shax3/square/domain/debate/controller/DebateController.java
+++ backend/src/main/java/org/shax3/square/domain/debate/controller/DebateController.java
@@ -1,7 +1,15 @@
 package org.shax3.square.domain.debate.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.shax3.square.domain.auth.annotation.AuthUser;
+import org.shax3.square.domain.debate.dto.request.VoteRequest;
+import org.shax3.square.domain.debate.dto.response.VoteResponse;
 import org.shax3.square.domain.debate.service.DebateService;
+import org.shax3.square.domain.user.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class DebateController {
     private final DebateService debateService;
+
+
+
+    @PostMapping("/vote")
+    public ResponseEntity<VoteResponse> vote(@Valid @RequestBody VoteRequest request, @AuthUser User user) {
+
+        VoteResponse response = debateService.vote(request, user);
+        return ResponseEntity.ok(response);
+    }
 }
