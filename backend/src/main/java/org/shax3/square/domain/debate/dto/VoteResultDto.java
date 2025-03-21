5a8b660682c495225ef6d9954ce08c1feb43b459
sungwook <respectwo@naver.com>
2025-03-21 13:14:59 +0900

    :bug: fix: voteresultdto 삭제

--- backend/src/main/java/org/shax3/square/domain/debate/dto/VoteResultDto.java
+++
@@ -1,8 +0,0 @@
-package org.shax3.square.domain.debate.dto;
-
-public record VoteResultDto(int leftVotes, int rightVotes) {
-    public int totalVotes() {
-        return leftVotes + rightVotes;
-    }
-}
-
