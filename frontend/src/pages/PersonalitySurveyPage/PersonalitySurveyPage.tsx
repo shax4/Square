b4a69793452621ad1839fb84a56c7f385546e9cb
김민철 <alscjf0327@naver.com>
2025-03-24 09:39:08 +0900

    ✨ feat: 설문조사 테스트 페이지 추가.

---
+++ frontend/src/pages/PersonalitySurveyPage/PersonalitySurveyPage.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function PersonalitySurveyPage() {
+    return (
+        <View style={styles.container}>
+            <Text>This Is PersonalitySurveyPage Screen.</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
