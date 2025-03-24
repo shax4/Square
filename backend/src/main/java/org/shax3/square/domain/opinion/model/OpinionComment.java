329eada1bc5af91406c947d288239dcfc25cdfe8
sungwook <respectwo@naver.com>
2025-03-24 17:02:32 +0900

    :recycle: refactor: basetimeentity 추가

--- backend/src/main/java/org/shax3/square/domain/opinion/model/OpinionComment.java
+++ backend/src/main/java/org/shax3/square/domain/opinion/model/OpinionComment.java
@@ -1,22 +1,26 @@
 package org.shax3.square.domain.opinion.model;
+
 import jakarta.persistence.*;
-import lombok.*;
-import org.shax3.square.domain.debate.model.Debate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.shax3.square.common.entity.BaseTimeEntity;
 import org.shax3.square.domain.user.model.User;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Table(name = "opinioncomment")
-public class OpinionComment {
+@Table(name = "opinionComment")
+public class OpinionComment extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name ="opinion_id",nullable = false)
+    @JoinColumn(name = "opinion_id", nullable = false)
     private Opinion opinion;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -29,9 +33,8 @@ public class OpinionComment {
     @Column(name = "like_count", nullable = false)
     private int likeCount;
 
-    @Column(name ="is_valid",nullable = false)
-    private boolean isValid;
-
+    @Column(name = "is_valid", nullable = false)
+    private boolean valid;
 
 
 }
