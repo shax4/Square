eda1f326a2199b2b8304c80075035375131b4b6d
윤다은 <syoon4486@gmail.com>
2025-03-24 15:09:40 +0900

    :recycle: refactor: 이미지 유효성 검사추가,fileName -> s3Key

--- backend/src/main/java/org/shax3/square/SquareApplication.java
+++ backend/src/main/java/org/shax3/square/SquareApplication.java
@@ -2,8 +2,10 @@ package org.shax3.square;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SquareApplication {
 
     public static void main(String[] args) {
