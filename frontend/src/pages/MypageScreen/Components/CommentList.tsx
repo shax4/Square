5e3dc2aebbbdf0308bb0ff85890d1576ac25b01d
김민철 <alscjf0327@naver.com>
2025-03-26 17:29:50 +0900

    ♻️ refactor: 리스트 리팩토링링

---
+++ frontend/src/pages/MypageScreen/Components/CommentList.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, FlatList } from "react-native";
+import {mockComments} from "./mocks"
+import CommentCard from "./CommentCard"
+
+const CommentList = () => {
+    return (
+        <FlatList
+            data={mockComments}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+            <CommentCard
+                title={item.title}
+                content={item.content}
+                likeCount={item.likeCount}
+                isLiked={item.isLiked}
+                onPress={() => console.log(`Comment card pressed: ${item.id}`)}
+                onLikeToggle={(isLiked) => console.log(`Like toggled to ${isLiked} for comment ${item.id}`)}
+            />
+            )}
+            contentContainerStyle={styles.listContent}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    listContent: {
+        padding: 16,
+    },
+});
+
+export default CommentList;
