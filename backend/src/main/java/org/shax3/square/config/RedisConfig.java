e8ef7fac61491f6615180d038a4c91bb0903e19f
윤다은 <syoon4486@gmail.com>
2025-03-26 17:02:29 +0900

    :bug: fix: 캐시 레디스 연결 설정 수정

--- backend/src/main/java/org/shax3/square/config/RedisConfig.java
+++ backend/src/main/java/org/shax3/square/config/RedisConfig.java
@@ -3,8 +3,10 @@ package org.shax3.square.config;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,10 +16,12 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Bean
@@ -31,13 +35,36 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(
-            LettuceConnectionFactory connectionFactory,
-            @Value("${spring.data.redis.ttl-minutes}") long cacheTtlMinutes) {
+            @Qualifier("cacheRedisConnectionFactory") LettuceConnectionFactory connectionFactory,
+            @Value("${spring.cache.redis.ttl-minutes}") long cacheTtlMinutes) {
 
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(cacheTtlMinutes));
+                .entryTtl(Duration.ofMinutes(cacheTtlMinutes))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
+                );
 
         return RedisCacheManager.builder(connectionFactory).cacheDefaults(config).build();
+    }
+
+    @Bean(name = "cacheRedisConnectionFactory")
+    @Primary
+    public LettuceConnectionFactory cacheRedisConnectionFactory(
+            @Value("${spring.cache.redis.host}") String host,
+            @Value("${spring.cache.redis.port}") int port,
+            @Value("${spring.cache.redis.password}") String password) {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(RedisPassword.of(password));
+
+        LettucePoolingClientConfiguration poolingConfig = LettucePoolingClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(5))
+                .shutdownTimeout(Duration.ofMillis(100))
+                .build();
+
+        return new LettuceConnectionFactory(config, poolingConfig);
     }
 
 
