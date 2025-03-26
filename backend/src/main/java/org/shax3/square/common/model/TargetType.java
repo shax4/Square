683d9f0c522129f7efab25ad39f2a54d55d1e444
eenzzi <purelej99@gmail.com>
2025-03-26 15:21:21 +0900

    :sparkles: feat: 좋아요 엔티티 생성

--- backend/src/main/java/org/shax3/square/common/model/TargetType.java
+++ backend/src/main/java/org/shax3/square/common/model/TargetType.java
@@ -1,5 +1,10 @@
 package org.shax3.square.common.model;
 
 public enum TargetType {
-    DEBATE, POST
+    POST_COMMENT, // 댓글 or 대댓글
+    DEBATE,
+    POST,
+    PROPOSAL,
+    OPINION_COMMENT, // 답글
+    OPINION // 의견
 }
