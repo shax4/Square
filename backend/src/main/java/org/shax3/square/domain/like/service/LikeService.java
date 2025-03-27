36a73419df575e522f4b99231d2317991ef10972
eenzzi <purelej99@gmail.com>
2025-03-27 10:00:53 +0900

    :sparkles: feat: 좋아요 타입별로 처리

--- backend/src/main/java/org/shax3/square/domain/like/service/LikeService.java
+++ backend/src/main/java/org/shax3/square/domain/like/service/LikeService.java
@@ -6,7 +6,10 @@ import org.shax3.square.domain.like.model.Like;
 import org.shax3.square.domain.like.repository.LikeRepository;
 import org.shax3.square.domain.opinion.service.OpinionCommentService;
 import org.shax3.square.domain.opinion.service.OpinionService;
+import org.shax3.square.domain.proposal.service.ProposalService;
 import org.shax3.square.domain.user.model.User;
+import org.shax3.square.exception.CustomException;
+import org.shax3.square.exception.ExceptionCode;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
@@ -19,13 +22,17 @@ public class LikeService {
 	private final LikeRepository likeRepository;
 	private final OpinionService opinionService;
 	private final OpinionCommentService opinionCommentService;
+	private final ProposalService proposalService;
 
 	@Transactional
 	public void like(User user, LikeRequest likeRequest) {
 
 		Long targetId = likeRequest.targetId();
-
 		TargetType targetType = likeRequest.targetType();
+
+		if (!isPresent(targetId, targetType)) {
+			throw new CustomException(ExceptionCode.NOT_FOUND);
+		}
 
 		Like like = likeRepository.findByUserAndTargetIdAndTargetType(user, targetId, targetType)
 			.map(existingLike -> {
@@ -38,12 +45,14 @@ public class LikeService {
 	}
 
 	public boolean isPresent(Long targetId, TargetType targetType) {
-		switch (targetType) {
-			case OPINION -> opinionService.getOpinion(targetId);
-			case OPINION_COMMENT -> opinionCommentService.getOpinionComment(targetId);
-			case PROPOSAL ->
-
-		}
+		return switch (targetType) {
+			case OPINION -> opinionService.isOpinionExists(targetId);
+			case OPINION_COMMENT -> opinionCommentService.isOpinionCommentExists(targetId);
+			case PROPOSAL -> proposalService.isProposalExists(targetId);
+			case POST -> false; // TODO: post 개발 후 구현
+			case POST_COMMENT -> false;
+			default -> throw new CustomException(ExceptionCode.INVALID_TARGET_TYPE);
+		};
 	}
 
 }
