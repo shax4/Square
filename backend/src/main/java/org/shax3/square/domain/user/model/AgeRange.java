8acd4cf5159835129bbc612883158dfcb9cc1819
sungwook <respectwo@naver.com>
2025-03-28 14:08:20 +0900

    :recycle: refactor: enum 한글 추가

--- backend/src/main/java/org/shax3/square/domain/user/model/AgeRange.java
+++ backend/src/main/java/org/shax3/square/domain/user/model/AgeRange.java
@@ -1,9 +1,22 @@
 package org.shax3.square.domain.user.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum AgeRange {
-    TEN,
-    TWENTY,
-    THIRTY,
-    FORTY,
-    FIFTY
+    TEN("10대"),
+    TWENTY("20대"),
+    THIRTY("30대"),
+    FORTY("40대"),
+    FIFTY("50대 이상");
+
+    private final String koreanName;
+
+    AgeRange(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    @JsonValue
+    public String getKoreanName() {
+        return koreanName;
+    }
 }
--- backend/src/main/java/org/shax3/square/domain/user/model/Gender.java
+++ backend/src/main/java/org/shax3/square/domain/user/model/Gender.java
@@ -1,8 +1,18 @@
 package org.shax3.square.domain.user.model;
 
-import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonValue;
 
-@Getter
 public enum Gender {
-    MALE, FEMALE, NONE
+    MALE("남성"),
+    FEMALE("여성"),
+    NONE("알리지 않음");
+
+    private final String koreanName;
+    Gender(String koreanName) {
+        this.koreanName = koreanName;
+    }
+    @JsonValue
+    public String getKoreanName() {
+        return koreanName;
+    }
 }
