58980ef8b8c533a3c61871e52a66ea4ec7ccb333
배석진 <bsj1044@naver.com>
2025-03-28 17:59:32 +0900

    ✨ feat: 텍스트 입력창 개발

--- frontend/src/pages/OpinionEditScreen/OpinionEditScreen.tsx
+++ frontend/src/pages/OpinionEditScreen/OpinionEditScreen.tsx
@@ -1,7 +1,7 @@
 import { RouteProp, useNavigation } from "@react-navigation/native";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { StackParamList } from "../../shared/page-stack/DebatePageStack";
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import colors from "../../../assets/colors";
 
@@ -15,13 +15,15 @@ export default function OpinionEditScreen({ route }: Props) {
     const submitRef = useRef<() => void>();
     const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>();
 
+    const [content, setContent] = useState("");
+
     // 수정 요청 함수
     const handleSubmit = () => {
         console.log('서버로 의견 수정 요청:', opinionId);
         // 요청 보내고 성공하면 이전 화면으로 이동
         navigation.goBack();
     };
-    
+
     // 이 submit 함수 자체를 외부에서 호출할 수 있게 ref에 저장
     submitRef.current = handleSubmit;
 
@@ -45,17 +47,44 @@ export default function OpinionEditScreen({ route }: Props) {
 
 
     return (
+        <View>
+            <View>
 
-        <Text>edit</Text>
+            </View>
+
+            <View>
+                {/* 내용 입력 영역 */}
+                <View style={styles.inputGroup}>
+                    <TextInput
+                        style={styles.contentInput}
+                        placeholder="내용을 입력하세요"
+                        value={content}
+                        onChangeText={setContent}
+                        multiline
+                        textAlignVertical="top" // 안드로이드에서 텍스트가 상단부터 시작하도록 설정
+                    />
+                </View>
+            </View>
+
+        </View>
     )
 }
 
 export const styles = StyleSheet.create({
     EditButton: {
-        
+
     },
     EditButtonText: {
         color: colors.primary,
         fontSize: 16,
-    }
-})
+    },
+    inputGroup: {
+        marginBottom: 20,
+    },
+    contentInput: {
+        margin: 15,
+        padding: 10,
+        fontSize: 16,
+        minHeight: 400, // 내용 입력 필드 최소 높이
+    },
+});
