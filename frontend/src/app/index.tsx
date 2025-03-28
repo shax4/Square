00e4cfb51b41e57d07f2c66a8e0242276339bbcc
김민철 <alscjf0327@naver.com>
2025-03-28 11:20:16 +0900

    🐛 fix: useAuth 대신 useAuthStore로 유저 정보 받아오게 변경.

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,24 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
 import BottomNav from './navigation/BottomNav';
+import { useEffect } from 'react';
+import { setupAuthInterceptor } from '../shared/api/authInterceptor';
 
 export default function App() {
+  useEffect(() => {
+    setupAuthInterceptor(); // 앱 시작 시 axios 설정에 유저 토큰 넣는 Interceptor 실행.
+  }, [])
+
   return (
     <BottomNav/>
   );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  screenContainer: {
-    flex: 5,
-    flexDirection: "row",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- frontend/src/shared/api/authInterceptor.ts
+++ frontend/src/shared/api/authInterceptor.ts
@@ -1,16 +1,18 @@
 import axiosInstance from "./axiosInstance";
+import {useAuthStore} from "../stores/auth"
 
-const getToken = () => {
-    return "my-token";
-}
+export const setupAuthInterceptor = () => {
+    axiosInstance.interceptors.request.use(
+        async (config) => {
+            const {user} = useAuthStore.getState();
 
-axiosInstance.interceptors.request.use(
-    async (config) => {
-        const token = await getToken();
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
+            console.log("[Axios Interceptor] setupAuthInterceptor 호출됨. 현재 유저 정보:", user);
+
+            if(user?.token){
+                config.headers.Authorization = `Bearer ${user.token}`;
+            }
+            return config;
+        },
+        (error) => Promise.reject(error)
+    );
+};
