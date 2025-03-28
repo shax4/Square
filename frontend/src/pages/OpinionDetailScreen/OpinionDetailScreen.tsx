4c1a07cf55e1f962e170fc5b0ed7507f35d17f72
배석진 <bsj1044@naver.com>
2025-03-28 10:19:02 +0900

    ✨ feat: 프로필 박스 공통 컴포넌트 개발

--- frontend/src/pages/OpinionDetailScreen/OpinionDetailScreen.tsx
+++ frontend/src/pages/OpinionDetailScreen/OpinionDetailScreen.tsx
@@ -1,7 +1,9 @@
 import { RouteProp, useRoute } from "@react-navigation/native";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { StackParamList } from "../../shared/page-stack/DebatePageStack";
 import opinionDetailTestData from './Components/opinion-detail-test-data';
+import ProfileBox from "../../components/ProfileBox/ProfileBox";
+import colors from "../../../assets/colors";
 
 type OpinionDetailRouteProp = RouteProp<StackParamList, 'OpinionDetailScreen'>;
 
@@ -9,12 +11,35 @@ export default function OpinionDetailScreen() {
 
     const route = useRoute<OpinionDetailRouteProp>();
     const { opinionId } = route.params;
-
+    const response = opinionDetailTestData;
     return (
-        <View>
-
+        <View style={styles.Container}>
+            <View style={styles.ProfileBoxView}>
+                <ProfileBox
+                    //imageUrl={response.profileUrl}
+                    nickname={response.nickname}
+                    userType={response.userType}
+                    createdAt={response.createdAt}
+                    variant="medium"
+                />
+            </View>
+            <View>
+                <Text>{response.content}</Text>
+            </View>
 
         </View>
 
+
     )
 }
+
+export const styles = StyleSheet.create({
+    Container: {
+        flex: 1,
+        backgroundColor: colors.white,
+    },
+    ProfileBoxView: {
+        flex: 1,
+        margin: 12,
+    },
+});
