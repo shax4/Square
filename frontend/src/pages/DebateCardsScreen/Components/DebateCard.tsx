c8ef0d4b7bb9c6efcfcf3d456b339057d793d76d
배석진 <bsj1044@naver.com>
2025-04-02 14:48:58 +0900

    ✨ feat: 북마크 버튼 컴포넌트 추가

--- frontend/src/pages/DebateCardsScreen/Components/DebateCard.tsx
+++ frontend/src/pages/DebateCardsScreen/Components/DebateCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
@@ -9,6 +9,7 @@ import { Debate } from './Debate.types';
 import { styles } from './DebateCard.styles';
 import { Icons } from '../../../../assets/icons/Icons';
 import VoteButton from '../../../components/VoteButton/VoteButton'
+import BookmarkButton from '../../../components/BookmarkButton/BookmarkButton';
 
 const DebateCard = ({
     debateId,
@@ -46,6 +47,13 @@ const DebateCard = ({
         navigation.navigate('OpinionListScreen', { debateId, debate });
     }
 
+    const [scrap, setScrap] = useState(debate.isScraped);
+
+    const handlePressScrap = () => {
+        console.log("DebateCard.tsx scrap Button Clicked")
+        setScrap(prev => !prev);
+    };
+
     return (
         <>
             <View style={styles.CardContainer}>
@@ -56,9 +64,10 @@ const DebateCard = ({
                     {/* Header */}
                     <View style={styles.CardHeader}>
                         <Text style={styles.CardHeaderText}>Number {debateId}</Text>
-                        <TouchableOpacity>
-                            {isScraped ? <Icons.bookmarkUndo on /> : <Icons.bookmark />}
-                        </TouchableOpacity>
+                        <BookmarkButton
+                            isScraped={debate.isScraped}
+                            onPressScrap={() => { handlePressScrap }}
+                        />
                     </View>
 
                     {/* Hashtag */}
