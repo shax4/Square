4634e8208158d581a24b6e835e1549e0d928bf66
김민철 <alscjf0327@naver.com>
2025-04-02 10:30:00 +0900

    ♻️ refactor: 마이페이지에서 내 성향 보기 버튼 관련 리팩토링.

--- frontend/src/shared/page-stack/MyPageStack.ts
+++ frontend/src/shared/page-stack/MyPageStack.ts
@@ -2,7 +2,7 @@ import { TypeResult } from "../types/typeResult";
 
 export type StackParamList = {
     // isAfterSurvey - 설문조사 이후 나오는 성향 결과 페이지인가? givenNickname - API 호출 시 사용되는 닉네임 정보, typeResult - isAfterSurvey가 true일 때 넘겨지는 유저 성향 데이터. 
-    PersonalityResultScreen: { isAfterSurvey : boolean, givenNickname : string, typeResult : TypeResult};
+    PersonalityResultScreen: { isAfterSurvey : boolean, givenNickname : string, typeResult : TypeResult | null};
     ProfileUpdateScreen: undefined;
     MypageScreen: undefined;
     DeleteAccountScreen: undefined;
