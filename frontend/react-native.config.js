18ddde5a1a641f33113683f2ef8c0b8de72cf9ca
sungwook <respectwo@naver.com>
2025-04-09 12:50:00 +0900

    feat: native auto linking 해결

---
+++ frontend/react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dependencies: {
+      '@react-native-firebase/auth': {},
+      '@react-native-firebase/app': {},
+      'react-native-svg': {},
+      '@react-native-google-signin/google-signin': {},
+    },
+};
+  
