d991415fbd2c0b237a15f5046e98a288ed4adada
eenzzi <purelej99@gmail.com>
2025-04-09 13:28:47 +0900

    :recycle: refactor: 프롬프트 내용 변경

--- backend/src/main/java/org/shax3/square/domain/debate/AiSummaryClient.java
+++ backend/src/main/java/org/shax3/square/domain/debate/AiSummaryClient.java
@@ -25,14 +25,14 @@ public class AiSummaryClient {
 			"%s" 주제에 대해 %s 측과 %s 측의 대표적인 주장 3개씩 총 6개를 추출해야돼.
 			
 			너의 응답은 항상 아래의 형식을 따라야해.
-			- 예시 형식: 주장1/ 주장2/ 주장3/ 주장4/ 주장5/ 주장6
+			- 예시 응답: %s: 주장1내용/ 주장2내용/ 주장3내용/ %s: 주장4내용/ 주장5내용/ 주장6내용
 			- 예시처럼 6개의 주장을 띄어쓰기 없이 슬래시(/)로만 구분해서 작성해줘.
 			- 전체 응답에 띄어쓰기나 번호, 슬래시 외의 기호가 들어가지 않게 작성해줘.
 			- 여기서 하나의 주장을 100자 정도로 작성해줘.
 			- 문장은 너무 공격적이지 않고 객관적인 표현으로 구성해줘.
 			- 먼저 %s 측 3개, 그 다음 %s 측 3개를 출력해줘.
 			""",
-			topic, leftOption, rightOption, leftOption, rightOption
+			topic, leftOption, rightOption, leftOption, rightOption, leftOption, rightOption
 		);
 	}
 }
