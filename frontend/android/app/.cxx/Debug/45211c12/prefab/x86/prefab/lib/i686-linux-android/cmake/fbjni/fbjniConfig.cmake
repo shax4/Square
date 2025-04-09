bb69d3f0bd8ed617fda8dda45e6aae3b8f40df8e
배석진 <bsj1044@naver.com>
2025-04-09 20:32:04 +0900

    ♻️ refactor: 안드로이드 빌드 폴더 경로 삭제

--- frontend/android/app/.cxx/Debug/45211c12/prefab/x86/prefab/lib/i686-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "C:/Users/SSAFY/.gradle/caches/8.10.2/transforms/cc6538d1dfd58568ef78eef8c5ee9673/transformed/fbjni-0.6.0/prefab/modules/fbjni/libs/android.x86/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/SSAFY/.gradle/caches/8.10.2/transforms/cc6538d1dfd58568ef78eef8c5ee9673/transformed/fbjni-0.6.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- frontend/android/app/.cxx/Debug/45211c12/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/SSAFY/.gradle/caches/8.10.2/transforms/d8197d9eced7013980f2241d373d872c/transformed/hermes-android-0.76.7-debug/prefab/modules/libhermes/libs/android.x86/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/SSAFY/.gradle/caches/8.10.2/transforms/d8197d9eced7013980f2241d373d872c/transformed/hermes-android-0.76.7-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
