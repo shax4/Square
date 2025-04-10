eb777dd3b516957a00fbdc84ee5f90d1550a7a3f
배석진 <bsj1044@naver.com>
2025-04-10 11:41:00 +0900

    ♻️refactor: 주제 등록 페이지 입력창 키보드 반응하도록 수정

--- frontend/src/app/navigation/NavigationData.ts
+++ frontend/src/app/navigation/NavigationData.ts
@@ -1,1 +1,12 @@
-export const hideTabBarScreens = ["PersonalitySurveyPage", "BoardDetail", "BoardWrite", "OpinionListScreen", "OpinionDetailScreen", "OpinionEditScreen", "ProposalCreateScreen", "SignupScreen", "ProposalListScreen"]; // 하단 네비게이션 바를 안보이게 할 스크린 이름.
+export const hideTabBarScreens = [
+    "PersonalitySurveyPage", 
+    "BoardDetail", 
+    "BoardWrite", 
+    "OpinionListScreen", 
+    "OpinionDetailScreen", 
+    "OpinionEditScreen", 
+    "ProposalCreateScreen", 
+    "SignupScreen", 
+    "ProposalListScreen", 
+    "ProposalEditScreen",
+    "ProfileUpdateScreen"]; // 하단 네비게이션 바를 안보이게 할 스크린 이름.
